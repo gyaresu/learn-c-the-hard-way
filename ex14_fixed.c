f5eaed1411929ee222a1e62b850d3ed02a59535c
Gareth <me@gareth.codes>
2015-09-23 22:31:26 -0700

    fixed 14

--- ex14_fixed.c
+++ ex14_fixed.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 // forward declarations
 int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(int chlen, char arg[]);
 
 void print_arguments(int argc, char *argv[]) {
     int i = 0;
+    int chlen = 0;
 
     for (i = 0; i < argc; ++i) {
-        print_letters(argv[i]);
+        chlen = strlen(argv[i]);
+        print_letters(chlen, argv[i]);
     }
 }
 
-void print_letters(char arg[]) {
+void print_letters(int chlen, char arg[]) {
     int i = 0;
 
-    for (i = 0; arg[i] != '\0'; ++i) {
+    for (i = 0; i < chlen; ++i) {
         char ch = arg[i];
 
-        if (can_print_it(ch)) {
+        if (isalpha(ch) || isblank(ch)) {
             printf("'%c' == %d ", ch, ch);
         }
     }
 
     printf("\n");
-}
-
-int can_print_it(char ch) {
-    return isalpha(ch) || isblank(ch);
 }
 
 int main(int argc, char *argv[]) {
