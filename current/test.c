6c67ffda24878719ebc1e9deeff6ab13128e7820
Gareth <me@gareth.codes>
2016-12-23 09:37:09 -0800

    test

--- current/test.c
+++ current/test.c
@@ -2,19 +2,35 @@
 
 int main(int argc, char *argv[])
 {
-    char *list[3] = {"Thing", "Stuff", "Wooble"};
+//    char *list[3] = {"Thing", "Stuff", "Wooble"};
+//
+//    int i = 0;
+//    int count = sizeof(list) / sizeof(list[0]);
+//
+//    for (i = 0; i < count; i++) {
+//        printf("%i from list is %s\n", i, list[i]);
+//        printf("Count is %d, i is %i.\n", count, i);
+//    }
+//
+//    for (i = 1; i < argc; i++) {
+//        printf("Argument %i is %s\n", i, argv[i]);
+//    }
 
-    int i = 0;
-    int count = sizeof(list) / sizeof(list[0]);
+    int a = 42;
+    int *b = &a;
+    double c = sizeof(b);
 
-    for (i = 0; i < count; i++) {
-        printf("%i from list is %s\n", i, list[i]);
-        printf("Count is %d, i is %i.\n", count, i);
-    }
+    printf("int i   == %i\n", a);
+    printf("int &i  == %pn\n", &a);
+    printf("int b   == %pn\n", b);
+    printf("int b+1 == %pn\n\n", b+1);
 
-    for (i = 1; i < argc; i++) {
-        printf("Argument %i is %s\n", i, argv[i]);
-    }
+    printf("sizeof(a) is %lu\n", sizeof(a));
+    printf("sizeof(&a) is %lu\n", sizeof(&a));
+    printf("sizeof(b) is %lu\n", sizeof(b));
+    printf("sizeof(b+1) is %lu\n\n", sizeof(b+1));
+
+    printf("double c is %f\n", c);
 
     return 0;
 }
